b0e0b83820da3a98067362ad0e532a6ab6af8e78
Volha Shut <Volha_Shut@epam.com>
2016-10-06 15:22:54 +0300

    add new tests

---
+++ test2.js
@@ -0,0 +1,65 @@
+describe('Onliner search', function() {
+    browser.ignoreSynchronization=true;
+    var EC=protractor.ExpectedConditions;
+    
+    it('title', function() {
+    	browser.get('https://www.onliner.by/');
+        browser.getTitle().then(function(title){
+        	expect(title).toBe('Onliner.by');
+        	
+        });
+    });
+
+    it('search', function() {
+        element(by.name('query')).sendKeys(" ");
+        browser.switchTo().frame(element(by.css('.modal-iframe')).getWebElement());     
+        browser.wait(EC.visibilityOf($('.search__input')),15000);
+        browser.findElement(by.css('.search__input')).sendKeys("iphone 6s 16gb Silver");
+        browser.wait(EC.visibilityOf($('.search__result')),20000);
+
+        var items=element.all(by.css('.search__results li'))
+          .then(function(items){
+              items[0].click();
+          })
+        browser.wait(EC.visibilityOf($('.g-bottom')),15000);
+    });
+
+    it ('iphone',function(){
+        browser.findElement(by.css('.product-specs__table'));
+        var cpu=element.all(by.css('.value__text')).getText()
+          .then(function(cpu){
+              expect(cpu[8]).toEqual("12 Мп");
+              expect(cpu[9]).toEqual("Apple A9");
+              expect(cpu[15]).toEqual("138.3 мм");
+          });
+          
+    });
+        
+    it("fmModule", function(){
+       browser.wait(EC.visibilityOf($('.product-main')), 20000); 
+        browser.findElement(by.css('tbody:nth-last-child(6) > tr:nth-child(3) > td:last-child > span'))
+		.getAttribute('class')
+		.then(function(fmModule){
+			expect(fmModule).toEqual( "i-x");
+		})
+		.then(function(){
+			console.log("+Test: FM-приёмника нет");}
+			,function (err) {
+    			console.error(err.message);
+    	    });
+        });
+
+    it("finge-print", function(){
+       browser.wait(EC.visibilityOf($('.product-main')), 20000); 
+        browser.findElement(by.css('tbody:nth-child(5) > tr:nth-child(12) > td:last-child > span'))
+		.getAttribute('class')
+		.then(function(fingerPrint){
+		expect(fmModule).toEqual( "i-tip");
+		})
+		.then(function(){
+			console.log("+Test: Сканер отпечатка пальца есть");}
+			,function (err) {
+    			console.error(err.message);
+    	});
+    });
+});
